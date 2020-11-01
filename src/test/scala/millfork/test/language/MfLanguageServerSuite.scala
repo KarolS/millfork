fd0ccfe8db2f2b490394d8514a7c9452f44160b7
Adam Gastineau <adam@appcannonsoftware.com>
2020-11-01 15:36:28 -0800

    Expanded initial hover tests

--- src/test/scala/millfork/test/language/MfLanguageServerSuite.scala
+++ src/test/scala/millfork/test/language/MfLanguageServerSuite.scala
@@ -1,63 +1,76 @@
 package millfork.test.language
 
-import millfork.error.Logger
-import millfork.{NullLogger, Platform}
 import org.scalatest.{AppendedClues, FunSuite, Matchers}
-import millfork.language.MfLanguageServer
-import millfork.Context
-import millfork.CompilationOptions
-import millfork.test.emu.{EmuPlatform, TestErrorReporting}
-import millfork.Cpu
-import millfork.JobContext
-import millfork.compiler.LabelGenerator
+import millfork.test.language.util._
 import org.eclipse.lsp4j.DidOpenTextDocumentParams
 import org.eclipse.lsp4j.TextDocumentItem
 import org.eclipse.lsp4j.HoverParams
 import org.eclipse.lsp4j.TextDocumentIdentifier
 import org.eclipse.lsp4j.Position
-import millfork.language.MfLanguageClient
 
 /**
   * @author Karol Stasiak
   */
 class MfLanguageServerSuite extends FunSuite with Matchers with AppendedClues {
   test("hover should find node under cursor, and its root declaration") {
-    implicit val logger: Logger = new NullLogger()
-    val platform = EmuPlatform.get(Cpu.Mos)
-    val jobContext = JobContext(TestErrorReporting.log, new LabelGenerator)
-    val server = new MfLanguageServer(
-      new Context(logger, List()),
-      new CompilationOptions(
-        platform,
-        Map(),
-        None,
-        0,
-        Map(),
-        EmuPlatform.textCodecRepository,
-        jobContext
-      )
-    )
+    val server = LanguageHelper.createServer
 
-    val textDocument = new TextDocumentItem("file.mfk", "millfork", 1, """
+    LanguageHelper.openDocument(
+      server,
+      "file.mfk",
+      """
       | byte test
+      | array(byte) foo[4]
       | void main() {
       |  test = test + 1
+      |  foo[1] = test
       | }
-    """.stripMargin)
-    val openParams = new DidOpenTextDocumentParams(textDocument)
-    server.textDocumentDidOpen(openParams)
-
-    val hoverParams = new HoverParams(
-      new TextDocumentIdentifier("file.mfk"),
-      new Position(3, 3)
+    """
     )
-    val response = server.textDocumentHover(hoverParams)
 
-    val hover = response.get
+    {
+      // Select `test` variable usage
+      val hoverParams = new HoverParams(
+        new TextDocumentIdentifier("file.mfk"),
+        new Position(4, 3)
+      )
+      val response = server.textDocumentHover(hoverParams)
+      val hover = response.get
 
-    val contents = hover.getContents().getRight()
-    contents should not equal (null)
-    contents.getValue() should equal("```mfk\nbyte test\n```")
+      val contents = hover.getContents().getRight()
+      contents should not equal (null)
+      contents.getValue() should equal(LanguageHelper.formatHover("byte test"))
+    }
+    {
+      // Select `main` function
+      val hoverParams = new HoverParams(
+        new TextDocumentIdentifier("file.mfk"),
+        new Position(3, 3)
+      )
+      val response = server.textDocumentHover(hoverParams)
+      val hover = response.get
+
+      val contents = hover.getContents().getRight()
+      contents should not equal (null)
+      contents.getValue() should equal(
+        LanguageHelper.formatHover("void main()")
+      )
+    }
+    {
+      // Select `foo` array usage
+      val hoverParams = new HoverParams(
+        new TextDocumentIdentifier("file.mfk"),
+        new Position(5, 6)
+      )
+      val response = server.textDocumentHover(hoverParams)
+      val hover = response.get
+
+      val contents = hover.getContents().getRight()
+      contents should not equal (null)
+      contents.getValue() should equal(
+        LanguageHelper.formatHover("array(byte) foo [4]")
+      )
+    }
   }
 
 }
